00cd42a997d7ad12406047709b71f5c54a0dcf4c
Francis Rodgers <benjamin-zipperer@web.de>
2019-03-17 22:22:30 +0100

    added second controller (empty)

---
+++ MUSIC_WEP_API/Areas/HelpPage/Controllers/SecondController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MUSIC_WEP_API.Areas.HelpPage.Controllers
+{
+    public class SecondController : ApiController
+    {
+    }
+}
